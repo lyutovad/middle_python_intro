49e58ff35b9ee733a356d63aacfae6ced033c5d2
Lyutova_D <gromovad@mail.ru>
2023-01-22 17:27:58 +0300

    Revert "errors in main.py and test_main.py found and changed"
    This reverts commit fe21bb5a9eb37935881689ea90390928ebb8a202.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 """Генератор приветствий."""
+import pprint
 
+def Greeting(name: str) -> str:
+      """Возвращает текст приветствия.
 
-def greeting(name: str) -> str:
-    """Возвращает текст приветствия.
+      Args:
+          name: Имя пользователя
 
-    Args:
-        name: Имя пользователя
-
-    Returns:
-        str: Текст приветствия
-    """
-    return 'Привет, {0}'.format(name.title())
+      Returns:
+          int: Текст приветствия
+      """
+      # pprint.pprint(name.lower())
+      return 'Привет, name'
